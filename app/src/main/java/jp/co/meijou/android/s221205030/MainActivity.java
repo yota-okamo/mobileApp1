5dfa2b3c492affc409f4f7a153a4b51294f9a207
yota-okamo <221205030@ccmailg.meijo-u.ac.jp>
2023-09-05 13:38:58 +0900

    textcolor変更

--- app/src/main/java/jp/co/meijou/android/s221205030/MainActivity.java
+++ app/src/main/java/jp/co/meijou/android/s221205030/MainActivity.java
@@ -1,14 +1,68 @@
 package jp.co.meijou.android.s221205030;
 
+import static jp.co.meijou.android.s221205030.R.drawable.baseline_ondemand_video_24;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.TextView;
+
+import jp.co.meijou.android.s221205030.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.change.setOnClickListener(view -> {
+            var text = binding.editText.getText().toString();
+            var text2 = binding.editText2.getText().toString();
+            binding.text.setText(text + text2);
+        });
+
+        binding.editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                binding.text.setText(s.toString());
+            }
+        });
+
+        binding.editText2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                binding.text.setText(s.toString());
+
+            }
+        });
+
+
     }
 }
